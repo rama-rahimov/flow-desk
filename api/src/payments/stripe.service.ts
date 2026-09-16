be16456af86f80114ea55c47c433ca9e95d4c279
rama-rahimov <r17654974@gmail.com>
2026-09-16 14:44:57 +0400

    update

--- api/src/payments/stripe.service.ts
+++ api/src/payments/stripe.service.ts
@@ -3,6 +3,7 @@ import { Stripe } from 'stripe';
 import dotenv from 'dotenv';
 import * as process from 'node:process';
 import {CreateCheckoutDto} from "./dto/create-checkout.dto.js";
+import {UpdateSubDto} from "./dto/update_subscription.dto.js";
 dotenv.config();
 
 @Injectable()
@@ -37,5 +38,11 @@ export class StripeService {
       success_url: 'http://localhost:5173/payment/success',
       cancel_url: 'http://localhost:5173/payment/cancel',
     });
+  }
+
+  async updateSub(data: UpdateSubDto) {
+    console.log({data});
+    return await this.stripe.subscriptions.update(data.sub_id, {cancel_at_period_end: data.cancel_at_period_end,
+      metadata:{paymentId: data.paymentId, companyId: data.companyId, cancelAtPeriodEnd: data.cancel_at_period_end?1:0}});
   }
 }
